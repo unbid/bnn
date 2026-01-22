db6c29deb266e57548264ffd6b73774caa36d908
20031207 <99nakia@gmail.com>
2026-01-22 16:00:46 +0100

    ciao

--- app.js
+++ app.js
@@ -75,6 +75,7 @@ var notizia26Router = require('./routes/notizia26');
 var notizia27Router = require('./routes/notizia27');
 var notizia28Router = require('./routes/notizia28');
 var notizia29Router = require('./routes/notizia29');
+var notizia30Router = require('./routes/notizia30');
 
 
 
@@ -145,6 +146,7 @@ app.use('/notizia26', notizia26Router);
 app.use('/notizia27', notizia27Router);
 app.use('/notizia28', notizia28Router);
 app.use('/notizia29', notizia29Router);
+app.use('/notizia30', notizia30Router);
 
 
 // catch 404 and forward to error handler
